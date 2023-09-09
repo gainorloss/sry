bd53a6b91f8fb1b45d1d1a1b3388964935d25150
gs <519564415@qq.com>
2023-09-09 20:15:00 +0800

    !feat:zipkin

---
+++ samples/Sample.API/Extensions/OpenTelemetryServiceCollectionExtensions.cs
@@ -0,0 +1,55 @@
+﻿using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class OpenTelemetryServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOpenTelemetry(this IServiceCollection services, IConfiguration configuration, string serviceName)
+        {
+
+            var tracingOtlpEndpoint = configuration["OpenTelemetry:OTLP_ENDPOINT_URL"];
+            var zipkinEndpoint = configuration["OpenTelemetry:ZIPKIN_ENDPOINT_URL"];
+            var otel = services.AddOpenTelemetry();
+
+            // Configure OpenTelemetry Resources with the application name
+            otel.ConfigureResource(resource => resource
+                .AddService(serviceName));
+
+            // Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+            otel.WithMetrics(metrics => metrics
+                // Metrics provider from OpenTelemetry
+                .AddAspNetCoreInstrumentation()
+                //.AddMeter(greeterMeter.Name)
+                // Metrics provides by ASP.NET Core in .NET 8
+                .AddMeter("Microsoft.AspNetCore.Hosting")
+                .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+                .AddPrometheusExporter());
+
+            // Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
+            otel.WithTracing(tracing =>
+            {
+                tracing.AddAspNetCoreInstrumentation();
+                tracing.AddHttpClientInstrumentation();
+                //tracing.AddSource(greeterActivitySource.Name);
+                tracing.AddConsoleExporter();
+                if (tracingOtlpEndpoint != null)
+                {
+                    tracing.AddOtlpExporter(otlpOptions =>
+                    {
+                        otlpOptions.Endpoint = new Uri(tracingOtlpEndpoint);
+                    });
+                }
+                if (zipkinEndpoint != null)
+                {
+                    tracing.AddZipkinExporter(opt =>
+                    {
+                        opt.Endpoint = new Uri($"{zipkinEndpoint}/api/v2/spans");
+                    });
+                }
+            });
+            return services;
+        }
+    }
+}
--- samples/Sample.API/Program.cs
+++ samples/Sample.API/Program.cs
@@ -1,7 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 using Sample.API.Core;
 
 
@@ -14,44 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
-var tracingOtlpEndpoint = builder.Configuration["OTLP_ENDPOINT_URL"];
-var otel = builder.Services.AddOpenTelemetry();
-
-// Configure OpenTelemetry Resources with the application name
-otel.ConfigureResource(resource => resource
-    .AddService(serviceName: $"{builder.Environment.ApplicationName}.{builder.Environment.EnvironmentName}"));
-
-// Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
-otel.WithMetrics(metrics => metrics
-    // Metrics provider from OpenTelemetry
-    .AddAspNetCoreInstrumentation()
-    //.AddMeter(greeterMeter.Name)
-    // Metrics provides by ASP.NET Core in .NET 8
-    .AddMeter("Microsoft.AspNetCore.Hosting")
-    .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
-    .AddPrometheusExporter());
-
-// Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
-otel.WithTracing(tracing =>
-{
-    tracing.AddAspNetCoreInstrumentation();
-    tracing.AddHttpClientInstrumentation();
-    //tracing.AddSource(greeterActivitySource.Name);
-    if (tracingOtlpEndpoint != null)
-    {
-        tracing.AddOtlpExporter(otlpOptions =>
-        {
-            otlpOptions.Endpoint = new Uri(tracingOtlpEndpoint);
-        });
-    }
-    else
-    {
-        tracing.AddConsoleExporter();
-    }
-});
-
+builder.Services.AddOpenTelemetry(builder.Configuration, $"{builder.Environment.ApplicationName}.{builder.Environment.EnvironmentName}");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
